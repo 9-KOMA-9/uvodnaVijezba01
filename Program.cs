48de3ff48a26645d9d1a1733bc484c6cec48cb3a
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-09-20 08:45:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijablaIspisPodataka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            a = 100;
+            b = 3;
+            x = a / b;
+            y = a % b;
+            Console.WriteLine(a + " djeljeno sa " + b +
+                " je " + x + " s ostatkom " + y);
+            Console.ReadKey();
+        }
+    }
+}
